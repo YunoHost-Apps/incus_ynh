3ac4fe21f67da34936915836d9be075038baf9a5
Félix Piédallu <felix@piedallu.me>
2024-03-14 22:22:49 +0100

    Update package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,29 @@
 # PERSONAL HELPERS
 #=================================================
 
+_ynh_add_dnsmasq_config() {
+    ynh_add_config --template="dnsmasq.conf" --destination="/etc/dnsmasq.d/$app"
+    ynh_systemd_action --service=dnsmasq --action=restart --log_path=systemd
+}
+
+_ynh_remove_dnsmasq_config() {
+    ynh_secure_remove --file="/etc/dnsmasq.d/$app"
+    ynh_systemd_action --service=dnsmasq --action=restart --log_path=systemd
+}
+
+
+_ynh_add_subuid_subgid() {
+    subuid_string="# Added for Incus\nroot:100000:65536"
+    echo -e "$subuid_string" > /etc/subuid
+    echo -e "$subuid_string" > /etc/subgid
+}
+
+_ynh_remove_subuid_subgid() {
+    sed -i "/# Added for Incus$/{N;/root:100000:65536/d}" /etc/subuid
+    sed -i "/# Added for Incus$/{N;/root:100000:65536/d}" /etc/subgid
+}
+
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
