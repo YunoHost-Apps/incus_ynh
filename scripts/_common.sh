42c3f7cd84fc456fe283626262be7ce3697ccc4d
Félix Piédallu <felix@piedallu.me>
2024-10-13 13:39:27 +0200

    Firewall tweak was moved from yunorunner package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -30,6 +30,18 @@ _ynh_remove_subuid_subgid() {
     sed -i "/# Added for Incus$/{N;/root:100000:65536/d}" /etc/subgid
 }
 
+_ynh_firewall_add_tweak() {
+    mkdir -p "/etc/yunohost/hooks.d/post_iptable_rules"
+
+    ynh_add_config --template="firewall_rules.sh" --destination="/etc/yunohost/hooks.d/post_iptable_rules/50-${app}"
+    yunohost firewall reload
+}
+
+_ynh_firewall_remove_tweak() {
+    ynh_secure_remove --file="/etc/yunohost/hooks.d/post_iptable_rules/50-${app}"
+    yunohost firewall git remote add origin git@github.com:user/repository.git
+}
+
 
 #=================================================
 # EXPERIMENTAL HELPERS
