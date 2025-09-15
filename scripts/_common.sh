ede35fd7397bd24629279637ceab2d40242a9d9b
Félix Piédallu <felix@piedallu.me>
2025-09-15 13:19:03 +0200

    Use an nftables config file instead of post_iptable_rules. Rename functions for coherency with helpers v2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,35 +18,33 @@ _set_incus_bridge_ip() {
 }
 
 
-_ynh_add_dnsmasq_config() {
+_ynh_config_add_dnsmasq() {
     ynh_config_add --template="dnsmasq.conf" --destination="/etc/dnsmasq.d/$app"
     ynh_systemctl --service=dnsmasq --action=restart --log_path=systemd
 }
 
-_ynh_remove_dnsmasq_config() {
+_ynh_config_remove_dnsmasq() {
     ynh_safe_rm "/etc/dnsmasq.d/$app"
     ynh_systemctl --service=dnsmasq --action=restart --log_path=systemd
 }
 
-_ynh_add_subuid_subgid() {
+_ynh_config_add_subuid_subgid() {
     subuid_string="# Added for Incus\nroot:100000:65536"
     echo -e "$subuid_string" > /etc/subuid
     echo -e "$subuid_string" > /etc/subgid
 }
 
-_ynh_remove_subuid_subgid() {
+_ynh_config_remove_subuid_subgid() {
     sed -i "/# Added for Incus$/{N;/root:100000:65536/d}" /etc/subuid
     sed -i "/# Added for Incus$/{N;/root:100000:65536/d}" /etc/subgid
 }
 
-_ynh_firewall_add_tweak() {
-    mkdir -p "/etc/yunohost/hooks.d/post_iptable_rules"
-
-    ynh_config_add --template="firewall_rules.sh" --destination="/etc/yunohost/hooks.d/post_iptable_rules/50-${app}"
+_ynh_config_add_nftables() {
+    ynh_config_add --template="nftables.conf" --destination="/etc/nftables.d/${app}.conf"
     yunohost firewall reload
 }
 
-_ynh_firewall_remove_tweak() {
-    ynh_safe_rm "/etc/yunohost/hooks.d/post_iptable_rules/50-${app}"
-    yunohost firewall git remote add origin git@github.com:user/repository.git
+_ynh_config_remove_nftables() {
+    ynh_safe_rm "/etc/nftables.d/${app}.conf"
+    yunohost firewall reload
 }
